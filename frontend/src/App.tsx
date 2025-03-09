9f101a7ca632c90895021c2e269ec38c0cbbdd79
Holland Blumer <63265395+hollandblumer6@users.noreply.github.com>
2025-03-09 11:25:29 -0400

    changed api url

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_URL = "https://what-dog-are-you.onrender.com"; // Backend URL
+const API_URL = import.meta.env.VITE_API_URL; // Use environment variable
 
 function App() {
   const [breed, setBreed] = useState("");
